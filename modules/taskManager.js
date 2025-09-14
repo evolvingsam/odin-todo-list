5b72693221a4042bf88d7b7c783784066d1ce6a7
samuel <alawodesamuel41@gmail.com>
2025-09-14 23:53:04 +0100

    completed the task manager

--- modules/taskManager.js
+++ modules/taskManager.js
@@ -35,4 +35,48 @@ export default class TodoManager {
         // return this.#taskCount - 1;
     
     }
+
+    getTask(projectId, taskId) {
+        let task = this.#projects[projectId].getTask(taskId);
+        return { ...task };
+    }
+    editTask(projectId, taskId, title, description, dueDate, priority) {
+        let task = this.#projects[projectId].getTask(taskId);
+        if (title !== null) task.title = title;
+        if (description !== null) task.description = description;
+        if (dueDate !== null) task.dueDate = dueDate;
+        if (priority !== null) task.priority = priority;
+
+    }
+    removeTask(projectId, taskId) {
+        this.#projects[projectId].removeTask(taskId);
+    }
+    completeTask(projectId, taskId) {
+        let task = this.#projects[projectId].getTask(taskId);
+        task.completed = true;
+    }
+    uncompleteTask(projectId, taskId) {
+        let task = this.#projects[projectId].getTask(taskId);
+        task.completed = false;
+    }
+
+    newProject(title) {
+        let project = new Project(title);
+        this.#projects[this.#projectCount++] = project;
+        return this.#projectCount - 1;
+    }
+    getProject(id) {
+        let project = this.#projects[id];
+        return { ...project };
+    }
+    editProject(id, title) {
+        let project = this.#projects[id];
+        if (title !== null) project.title = title;
+    }
+    removeProject(id) {
+        delete this.#projects[id];
+    }
+    getProjects() {
+        return this.#projects;
+    }
 }
