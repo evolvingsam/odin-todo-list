6a13333997408ad33ed840204baac4124005defe
samuel <alawodesamuel41@gmail.com>
2025-09-14 21:31:25 +0100

    created all modules

---
+++ modules/todo.js
@@ -0,0 +1,11 @@
+export default class todo {
+        description = null;
+        dueDate = null;
+        priority = null;
+        completed = false;
+
+    constructor(title) {
+        this.title = title;
+
+    }
+}
