6ab4e3f216db5cd8433aa124b91a0e4c92c3b18b
samuel <alawodesamuel41@gmail.com>
2025-09-22 19:51:02 +0100

    moved files to thier right places

--- modules/save-tasks.js
+++
@@ -1,8 +0,0 @@
-export default function saveTasks(taskManager) {
-  let saved = {
-    "taskCount": taskManager.taskCount,
-    "projectCount": taskManager.projectCount,
-    "projects": taskManager.getProjects()
-  }
-  localStorage.setItem("savedTasks", JSON.stringify(saved));
-}
